68a0850664b51a1adcb3e3955644b82f1b741c2d
Nilotpal Sanyal <nilotpal.sanyal@gmail.com>
2022-09-13 06:21:27 +0530

    Delete R directory

--- R/welcome_msg.R
+++
@@ -1,6 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  message <- c("\n Welcome to GWASinlps...Happy selection!",
-               "\n \n Website: https://nilotpalsanyal.github.io/GWASinlps/",
-               "\n Bug report: https://github.com/nilotpalsanyal/GWASinlps/issues")
-  packageStartupMessage(message)
-}
